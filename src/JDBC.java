a5d8ced5402648f8cceedc4fd7f03c9cfd7acc37
Tsafaras <kontsaf13@gmail.com>
2020-02-13 14:28:23 +0200

    more Listeners

--- src/JDBC.java
+++ src/JDBC.java
@@ -1,43 +1,62 @@
 import javax.swing.*;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBC
 {
-    public static Connection getConnection() throws SQLException {
-    Connection conn = null;
-    String url       = "jdbc:mysql://localhost:3306/test";
-    String user      = "con";
-    String password  = "";
+    public static Connection getConnection() throws SQLException
+    {
+        Connection conn;
+        String url       = "jdbc:mysql://localhost:3306/Newspaper";
+        String user      = "con";
+        String password  = "";
 
-    // create a connection to the database
-    conn = DriverManager.getConnection(url, user, password);
-
-    return conn;
+        // create a connection to the database
+        conn = DriverManager.getConnection(url, user, password);
+        return conn;
     }
 
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+    public static boolean logIn(String email, String password, String as, JPanel Main_Window) throws SQLException
+    {
+        Connection conn = getConnection();
+        boolean flag = true;
 
-        /*try (Connection conn = JDBC.getConnection()) {
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM " + as + " WHERE Email = '" + email + "'");
 
-            // print out a message
-            System.out.println(String.format("Connected to database %s "
-                    + "successfully.", conn.getCatalog()));
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }*/
+        if (!rs.next())
+        {
+            JOptionPane.showMessageDialog(Main_Window, "Wrong e-mail and/or selected job title.");
+            conn.close();
+            return false;
+        }
 
+        if (!as.equals("Publisher"))
+            as = "Employee";
+
+        stmt = conn.createStatement();
+        rs = stmt.executeQuery("SELECT * FROM " + as + " WHERE Email = '" + email + "' and Pass = '" + password + "'");
+
+        if (!rs.next())
+        {
+            JOptionPane.showMessageDialog(Main_Window, "Wrong e-mail and/or password.");
+            flag = false;
+        }
+
+        conn.close();
+        return flag;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+    {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Newspaper frame = new Newspaper();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        EventQueue.invokeLater(() ->
+        {
+            try {
+                Newspaper frame = new Newspaper();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
     }
--- src/Newspaper.java
+++ src/Newspaper.java
@@ -1,17 +1,21 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Newspaper extends JFrame{
     private JPanel Journalist;
-    private JButton button1;
+    private JButton Login_BTN;
     private JPasswordField Pswd_TXTFLD;
-    private JTextField Username_TXTFLD;
+    private JTextField E_mail_TXTFLD;
     private JCheckBox Show_psw_chkbx;
     private JPanel Main_Window;
     private JPanel Login_Screen;
     private JPanel Editor;
-    private JPanel Admin;
+    private JPanel Administrative;
     private JPanel Publisher;
-    private JLabel Username_Label;
+    private JLabel E_mail_Label;
     private JLabel Pswd_Label;
     private JTabbedPane tabbedPane1;
     private JTabbedPane tabbedPane3;
@@ -21,7 +25,7 @@ public class Newspaper extends JFrame{
     private JTextArea J_summary;
     private JTextField J_writers;
     private JTextField J_tags;
-    private JTextField J_pages;
+    private JFormattedTextField J_pages;
     private JTextField J_category;
     private JButton J_Submit_BTN;
     private JTextField J_Rev_path;
@@ -33,8 +37,7 @@ public class Newspaper extends JFrame{
     private JButton J_Rev_Submit_BTN;
     private JTextField E_newCategory;
     private JTextField A_sheet_returned;
-    private JTextField A_fromMonth;
-    private JTextField A_toMonth;
+    private JTextField A_months;
     private JButton A_Payroll_OKBTN;
     private JTextField J_images;
     private JTextField J_Rev_Images;
@@ -48,9 +51,36 @@ public class Newspaper extends JFrame{
     private JButton E_Submit_Article_BTN;
     private JButton E_Category_Submit_BTN;
     private JButton A_returned_Submit_BTN;
+    private JButton E_logoutBTN;
+    private JLabel E_welcome;
+    private JButton A_logoutBTN;
+    private JButton P_logoutBTN;
+    private JButton J_logoutBTN;
+    private JLabel J_welcome;
+    private JLabel A_welcome;
+    private JLabel P_welcome;
+    private JRadioButton J_RDBTN;
+    private JRadioButton E_RDBTN;
+    private JRadioButton P_RDBTN;
+    private JRadioButton A_RDBTN;
+    private JTextField A_volume_returned;
+    private JTable table1;
+    private JTextField J_Title;
+    private JTextField J_Rev_Title;
+    private JTextField E_Title;
 
     private void switchPanel(JPanel panel)
     {
+        if ( panel == Login_Screen )
+        {
+            E_mail_TXTFLD.setText("");
+            Pswd_TXTFLD.setText("");
+            if (Show_psw_chkbx.isSelected())
+            {
+                Show_psw_chkbx.setSelected(false);
+                Pswd_TXTFLD.setEchoChar('*');
+            }
+        }
         Main_Window.removeAll();
         Main_Window.add(panel);
         Main_Window.repaint();
@@ -58,15 +88,47 @@ public class Newspaper extends JFrame{
         pack();
     }
 
+    private void login(String as, String email)
+    {
+        switch (as)
+        {
+            case "Journalist":
+                J_welcome.setText("Logged in as: " + email);
+                switchPanel(Journalist);
+                break;
+            case "Editor":
+                E_welcome.setText("Logged in as: " + email);
+                switchPanel(Editor);
+                break;
+            case "Administrative":
+                A_welcome.setText("Logged in as: " + email);
+                switchPanel(Administrative);
+                break;
+            case "Publisher":
+                P_welcome.setText("Logged in as: " + email);
+                switchPanel(Publisher);
+                break;
+
+        }
+    }
+
+    private void isDigit(KeyEvent e)
+    {
+        char c = e.getKeyChar();
+        if (!(Character.isDigit(c) || (c == KeyEvent.VK_BACK_SPACE) || (c == KeyEvent.VK_DELETE)))
+            e.consume();
+    }
+
     private void submit_article(String path, String summary, String category, String writers, String tags, int pages, String Images)
     {
         // sql submit article ---
     }
 
-    private void payroll(int from, int to)
+    private void payroll(int from)
     {
         // sql view payroll
     }
+
     public Newspaper(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(100, 100, 450, 300);
@@ -74,28 +136,29 @@ public class Newspaper extends JFrame{
         setContentPane(Main_Window);
         setTitle("Newspaper");
 
-        button1.addActionListener(actionEvent -> {
-            String username = Username_TXTFLD.getText();
-            String pswd = Pswd_TXTFLD.getText();
+        Login_BTN.addActionListener(actionEvent ->
+        {
+            switchPanel(Journalist);
+            /*String email = E_mail_TXTFLD.getText();
+            String password = Pswd_TXTFLD.getText();
+            boolean flag = false;
+            if (email.length()>0 && password.length()>0)
+            {
+                String as = Objects.requireNonNull(jobRole()).getText();
+                if (as != null)
+                try {
+                    flag = JDBC.logIn(email, password, as, Main_Window);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                else
+                    JOptionPane.showMessageDialog(Main_Window, "Please select the appropriate job role!");
 
-            switch (username) {
-                case "J":
-                    setTitle(getTitle() + " - Journalist");
-                    switchPanel(Journalist);
-                    break;
-                case "E":
-                    setTitle(getTitle() + " - Editor in Chief");
-                    switchPanel(Editor);
-                    break;
-                case "A":
-                    setTitle(getTitle() + " - Administrator");
-                    switchPanel(Admin);
-                    break;
-                case "P":
-                    setTitle(getTitle() + " - Publisher");
-                    switchPanel(Publisher);
-                    break;
+                if (flag) //credentials check out!
+                    login(as, email);
             }
+            else
+                JOptionPane.showMessageDialog(Main_Window, "Enter credentials first!");*/
         });
 
         J_Submit_BTN.addActionListener(actionEvent -> {
@@ -135,12 +198,89 @@ public class Newspaper extends JFrame{
             submit_article(path, summary, category, writers, tags, pages, images);
         });
 
-
         A_Payroll_OKBTN.addActionListener(actionEvent -> {
-            int from = Integer.parseInt(A_fromMonth.getText());
-            int to = Integer.parseInt(A_toMonth.getText());
+            int from = Integer.parseInt(A_months.getText());
 
-            payroll(from, to);
+            payroll(from);
         });
+
+        Show_psw_chkbx.addActionListener(actionEvent ->
+        {
+            if (Show_psw_chkbx.isSelected())
+                Pswd_TXTFLD.setEchoChar((char)0);
+            else
+                Pswd_TXTFLD.setEchoChar('*');
+
+        });
+
+        J_logoutBTN.addActionListener(actionEvent ->
+        {
+            switchPanel(Login_Screen);
+        });
+
+        E_logoutBTN.addActionListener(actionEvent ->
+        {
+            switchPanel(Login_Screen);
+        });
+
+        A_logoutBTN.addActionListener(actionEvent ->
+        {
+            switchPanel(Login_Screen);
+        });
+
+        P_logoutBTN.addActionListener(actionEvent ->
+        {
+            switchPanel(Login_Screen);
+        });
+
+        J_pages.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e){isDigit(e);}
+        });
+
+        J_Rev_pages.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {isDigit(e);}
+        });
+
+        E_pages.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {isDigit(e);}
+        });
+
+        A_months.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {isDigit(e);}
+        });
+
+        A_sheet_returned.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {isDigit(e);}
+        });
+
+        A_volume_returned.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {isDigit(e);}
+        });
+
+        A_returned_Submit_BTN.addActionListener(actionEvent ->
+        {
+            int sheet = Integer.parseInt(A_sheet_returned.getText());
+            int volume = Integer.parseInt(A_volume_returned.getText());
+            //
+        });
+    }
+
+    private JRadioButton jobRole()
+    {
+        if (J_RDBTN.isSelected())
+            return J_RDBTN;
+        else if (E_RDBTN.isSelected())
+            return E_RDBTN;
+        else if (A_RDBTN.isSelected())
+            return A_RDBTN;
+        else if (P_RDBTN.isSelected())
+            return P_RDBTN;
+        else return null;
     }
 }
