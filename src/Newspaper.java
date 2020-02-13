ffaee7bac6b4cb4b93882841e89424c6255ad2f1
Tsafaras <kontsaf13@gmail.com>
2020-02-13 23:49:04 +0200

    more Listeners

--- src/Newspaper.java
+++ src/Newspaper.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.sql.*;
+import java.util.Vector;
 
 public class Newspaper extends JFrame{
     private JPanel Journalist;
@@ -26,11 +27,11 @@ public class Newspaper extends JFrame{
     private JTextField J_writers;
     private JTextField J_tags;
     private JFormattedTextField J_pages;
-    private JTextField J_category;
+    private JComboBox J_category;
     private JButton J_Submit_BTN;
     private JTextField J_Rev_path;
     private JTextArea J_Rev_summary;
-    private JTextField J_Rev_category;
+    private JComboBox J_Rev_category;
     private JTextField J_Rev_writers;
     private JTextField J_Rev_tags;
     private JTextField J_Rev_pages;
@@ -43,7 +44,7 @@ public class Newspaper extends JFrame{
     private JTextField J_Rev_Images;
     private JTextField E_path;
     private JTextArea E_summary;
-    private JTextField E_category;
+    private JComboBox E_category;
     private JTextField E_writers;
     private JTextField E_tags;
     private JTextField E_pages;
@@ -64,10 +65,50 @@ public class Newspaper extends JFrame{
     private JRadioButton P_RDBTN;
     private JRadioButton A_RDBTN;
     private JTextField A_volume_returned;
-    private JTable table1;
+    private JTable A_Payroll_TBL;
     private JTextField J_Title;
     private JTextField J_Rev_Title;
     private JTextField E_Title;
+    private JLabel Total_Payroll;
+    private JScrollPane A_ScrollPane;
+    private JComboBox<String> E_CatParent;
+    private JTextField E_CatDescr;
+    private JComboBox<String> J_RevBox;
+    private JButton J_RevOK;
+    private JComboBox<String> J_ViewBox;
+    private JButton J_ViewOK;
+    private JLabel J_View_Path;
+    private JLabel J_View_Title;
+    private JLabel J_View_Summary;
+    private JLabel J_View_Category;
+    private JLabel J_View_Writers;
+    private JLabel J_View_Tags;
+    private JLabel J_View_Pages;
+    private JLabel J_View_Images;
+    private JComboBox<String> comboBox1;
+    private JTextArea textArea1;
+    private JComboBox<String> comboBox2;
+    private JRadioButton ACCEPTEDRadioButton;
+    private JRadioButton PENDINGRadioButton;
+    private JRadioButton REJECTEDRadioButton;
+    private JRadioButton TO_BE_REVISEDRadioButton;
+    private JButton submitButton;
+    private JTextField E_Approve_Order;
+    private JTextField E_Appear_Order;
+    private JComboBox<String> E_Order_Select;
+    private JButton submitButton1;
+    private JComboBox<String> P_NewspaperBox;
+    private JTextField P_Edit_Name;
+    private JRadioButton P_Daily;
+    private JRadioButton P_Monthly;
+    private JRadioButton P_Weekly;
+    private JComboBox<String> P_Amout_Sheet;
+    private JTextField P_Amount_Amount;
+    private JButton P_Edit_Submit;
+    private JButton P_Amount_Submit;
+    private JComboBox<String> P_EditorBox;
+    private JButton P_EditorSubmit;
+    private JTable P_SalesTBL;
 
     private void switchPanel(JPanel panel)
     {
@@ -88,7 +129,33 @@ public class Newspaper extends JFrame{
         pack();
     }
 
-    private void login(String as, String email)
+    private void updateQuery(String query) throws SQLException
+    {
+        // create a java mysql database connection
+        Connection conn = JDBC.getConnection();
+
+        // create the java mysql update preparedstatement
+        Statement stmt = conn.createStatement();
+
+        stmt.executeUpdate(query);
+
+        conn.close();
+    }
+
+    private void insertQuery(String query) throws SQLException
+    {
+        String email = E_mail_TXTFLD.getText();
+
+        // create a java mysql database connection
+        Connection conn = JDBC.getConnection();
+
+        // create the java mysql update preparedstatement
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(query);
+        conn.close();
+    }
+
+    private void login(String as, String email) throws SQLException
     {
         switch (as)
         {
@@ -98,6 +165,7 @@ public class Newspaper extends JFrame{
                 break;
             case "Editor":
                 E_welcome.setText("Logged in as: " + email);
+                //populate();
                 switchPanel(Editor);
                 break;
             case "Administrative":
@@ -119,17 +187,46 @@ public class Newspaper extends JFrame{
             e.consume();
     }
 
-    private void submit_article(String path, String summary, String category, String writers, String tags, int pages, String Images)
-    {
-        // sql submit article ---
+    private void payroll(int period) throws SQLException {
+        // create a java mysql database connection
+        Connection conn = JDBC.getConnection();
+
+        // create the java mysql update preparedstatement
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT Name, Surn, Employee.Email, Salary, FLOOR(DATEDIFF(CURDATE(), Recrdate)/30) Months, FLOOR(DATEDIFF(CURDATE(), Recrdate)/30 *Salary) Wage FROM Employee " +
+                "INNER JOIN Administrative ON Employee.Paper_Name = Administrative.Paper_Name " +
+                "WHERE Administrative.Email = '" + E_mail_TXTFLD.getText() + "' AND (FLOOR(DATEDIFF(CURDATE(), Recrdate)/30)) <= " + period);
+        ResultSetTableModel model = new ResultSetTableModel(rs);
+        A_Payroll_TBL.setModel(model);
+        conn.close();
     }
 
-    private void payroll(int from)
+    public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException
     {
-        // sql view payroll
+        ResultSetMetaData metaData = rs.getMetaData();
+
+        // names of columns
+        Vector<String> columnNames = new Vector<String>();
+        int columnCount = metaData.getColumnCount();
+        for (int column = 1; column <= columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
+        }
+
+        // data of the table
+        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        while (rs.next()) {
+            Vector<Object> vector = new Vector<Object>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                vector.add(rs.getObject(columnIndex));
+            }
+            data.add(vector);
+        }
+
+        return new DefaultTableModel(data, columnNames);
     }
 
-    public Newspaper(){
+    public Newspaper() throws SQLException
+    {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(100, 100, 450, 300);
         setLocationRelativeTo(null);
@@ -138,7 +235,13 @@ public class Newspaper extends JFrame{
 
         Login_BTN.addActionListener(actionEvent ->
         {
-            switchPanel(Journalist);
+            switchPanel(Administrative);
+            try {
+                populate(P_Amout_Sheet,"SELECT Sheet_Num from Sheet");
+                populate(P_EditorBox, "select Email from Employee");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             /*String email = E_mail_TXTFLD.getText();
             String password = Pswd_TXTFLD.getText();
             boolean flag = false;
@@ -161,47 +264,72 @@ public class Newspaper extends JFrame{
                 JOptionPane.showMessageDialog(Main_Window, "Enter credentials first!");*/
         });
 
+
+
         J_Submit_BTN.addActionListener(actionEvent -> {
             String path = J_path.getText();
+            String title = J_Title.getText();
             String summary = J_summary.getText();
-            String category = J_category.getText();
+            //String category = J_category.getText();
             String writers = J_writers.getText();
             String tags = J_tags.getText();
             int pages = Integer.parseInt(J_pages.getText());
             String images = J_images.getText();
 
-            submit_article(path, summary, category, writers, tags, pages, images);
+            String query = "INSERT INTO Article VALUES ('Article16', 1,'ARTHRO 1', '3', 'sum 1', 'ntiri@nyt.com',1,'ACCEPTED', 1, 'nyt', '2020-10-10',NULL)";
+
+            try {
+                insertQuery(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         });
 
         J_Rev_Submit_BTN.addActionListener(actionEvent -> {
             String path = J_Rev_path.getText();
+            String title = J_Rev_Title.getText();
             String summary = J_Rev_summary.getText();
-            String category = J_Rev_category.getText();
+            //String category = J_Rev_category.getText();
             String writers = J_Rev_writers.getText();
             String tags = J_Rev_tags.getText();
             int pages = Integer.parseInt(J_Rev_pages.getText());
             String images = J_Rev_Images.getText();
 
-            submit_article(path, summary, category, writers, tags, pages, images);
+            String query = "UPDATE ";
+            try {
+                updateQuery(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         );
 
         E_Submit_Article_BTN.addActionListener(actionEvent -> {
             String path = E_path.getText();
+            String title = E_Title.getText();
             String summary = E_summary.getText();
-            String category = E_category.getText();
+            //String category = E_category.getText();
             String writers = E_writers.getText();
             String tags = E_tags.getText();
             int pages = Integer.parseInt(E_pages.getText());
             String images = E_images.getText();
 
-            submit_article(path, summary, category, writers, tags, pages, images);
+            String query = "INSERT INTO Article VALUES";
+            try {
+                insertQuery(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         });
 
         A_Payroll_OKBTN.addActionListener(actionEvent -> {
-            int from = Integer.parseInt(A_months.getText());
+            int months = Integer.parseInt(A_months.getText());
 
-            payroll(from);
+            try {
+                payroll(months);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         });
 
         Show_psw_chkbx.addActionListener(actionEvent ->
@@ -214,24 +342,16 @@ public class Newspaper extends JFrame{
         });
 
         J_logoutBTN.addActionListener(actionEvent ->
-        {
-            switchPanel(Login_Screen);
-        });
+                switchPanel(Login_Screen));
 
         E_logoutBTN.addActionListener(actionEvent ->
-        {
-            switchPanel(Login_Screen);
-        });
+                switchPanel(Login_Screen));
 
         A_logoutBTN.addActionListener(actionEvent ->
-        {
-            switchPanel(Login_Screen);
-        });
+                switchPanel(Login_Screen));
 
         P_logoutBTN.addActionListener(actionEvent ->
-        {
-            switchPanel(Login_Screen);
-        });
+                switchPanel(Login_Screen));
 
         J_pages.addKeyListener(new KeyAdapter() {
             @Override
@@ -263,12 +383,60 @@ public class Newspaper extends JFrame{
             public void keyTyped(KeyEvent e) {isDigit(e);}
         });
 
+        E_Approve_Order.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {isDigit(e);}
+        });
+
+        E_Appear_Order.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {isDigit(e);}
+        });
+
         A_returned_Submit_BTN.addActionListener(actionEvent ->
         {
             int sheet = Integer.parseInt(A_sheet_returned.getText());
             int volume = Integer.parseInt(A_volume_returned.getText());
-            //
+            try {
+                updateQuery("update Sheet set Unsold_Copies = " + volume + " where Sheet_Num = " + sheet);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         });
+
+        E_Category_Submit_BTN.addActionListener(actionEvent ->
+        {
+            String category = E_newCategory.getText();
+            Object a = E_CatParent.getSelectedItem();
+            String parent = null;
+            if (a != null)
+                parent = E_CatParent.getSelectedItem().toString();
+            String description = E_CatDescr.getText();
+
+            try {
+                insertQuery("INSERT INTO Category VALUES (DEFAULT"+ ",(SELECT Cat_ID from Category WHERE Cat_Name='"+parent+"'),'" +category + "','" +description+ "')");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void populate(JComboBox<String> box, String que) throws SQLException
+    {
+        Connection conn = JDBC.getConnection();
+
+        // create the java mysql update preparedstatement
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(que);
+
+        box.addItem(null);
+
+        while (rs.next())
+        {
+            box.addItem(rs.getString(1));
+        }
+
+        conn.close();
     }
 
     private JRadioButton jobRole()
